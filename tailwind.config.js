464e202a0aa867ed02bf292536d39a7552b56e55
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-03-21 03:02:16 +0530

    Sort posts in descending order

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: {
+    files: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  },
   theme: {
     extend: {
       fontFamily: {
