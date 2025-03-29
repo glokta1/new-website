d847e640e828a65cbc3d35b7ae0393e6d373db68
Rafey Ahmad <rafeyahmad@protonmail.com>
2025-03-29 20:52:07 +0530

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+import defaultTheme from "tailwindcss/defaultTheme";
+import typography from "@tailwindcss/typography";
 
 export default {
   content: {
@@ -12,5 +13,5 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typography],
 };
