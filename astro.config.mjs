416f5beea08657ca3b09ff56826cf9c6b09ba84e
Rafey Ahmad <rafeyahmad@protonmail.com>
2026-08-02 02:10:16 +0530

    Revamp homepage and site details

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://new-website-two-gamma.vercel.app",
   image: {
     domains: ["cdn.hashnode.com"],
   },
